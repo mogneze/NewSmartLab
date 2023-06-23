ae2fa12f247d0764dd41761fbbd87aac5a5afa41
Vlad <93223622+mogneze@users.noreply.github.com>
2023-06-23 23:07:35 +0600

    chart saving and loading, font, back color

--- app/src/main/java/com/example/smartlab/activities/CreateChartActivity.java
+++ app/src/main/java/com/example/smartlab/activities/CreateChartActivity.java
@@ -4,27 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.text.format.DateUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smartlab.MainActivity;
 import com.example.smartlab.R;
+import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class CreateChartActivity extends AppCompatActivity {
 
-    String[] genders = {"М", "Ж"};
+    String[] genders = {"Мужской", "Женский"};
     TextView birthDate;
     Calendar dateAndTime = Calendar.getInstance();
-    EditText editTextAddress, editTextSurname, editTextLastname, editTextBirthDate;
+    EditText editTextAddress, editTextSurname, editTextLastname;
+    TextView editTextBirthDate;
     Spinner spinner;
+    Button buttonCreate;
+
+    SharedPreferences sharedPreferences;
+
+    ArrayList<String> arrPackage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,13 +49,45 @@ public class CreateChartActivity extends AppCompatActivity {
         spinner = findViewById(R.id.spinnerGender);
         ArrayAdapter<String> adapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, genders);
         spinner.setAdapter(adapter);
+
+        editTextAddress = findViewById(R.id.editTextAddress);
+        editTextSurname = findViewById(R.id.editTextSurname);
+        editTextLastname = findViewById(R.id.editTextLastname);
+        editTextBirthDate = findViewById(R.id.editTextBirthDate);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String addressText = editTextAddress.getText().toString();
+                String surnameText = editTextSurname.getText().toString();
+                String lastnameText = editTextLastname.getText().toString();
+                String birthdateText = editTextBirthDate.getText().toString();
+                buttonCreate = findViewById(R.id.buttonCreate);
+                if (!TextUtils.isEmpty(addressText) && !TextUtils.isEmpty(surnameText) && !TextUtils.isEmpty(lastnameText) && !TextUtils.isEmpty(birthdateText)) {
+                    buttonCreate.setBackground(getDrawable(R.drawable.rounded_button_active));
+                    buttonCreate.setEnabled(true);
+                }
+                else {
+                    buttonCreate.setBackground(getDrawable(R.drawable.rounded_button_inactive));
+                    buttonCreate.setEnabled(false);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        };
+        editTextAddress.addTextChangedListener(textWatcher);
+        editTextSurname.addTextChangedListener(textWatcher);
+        editTextLastname.addTextChangedListener(textWatcher);
+        editTextBirthDate.addTextChangedListener(textWatcher);
     }
+
     public void setDate(View v) {
         new DatePickerDialog(CreateChartActivity.this, listener,
                 dateAndTime.get(Calendar.YEAR),
                 dateAndTime.get(Calendar.MONTH),
-                dateAndTime.get(Calendar.DAY_OF_MONTH))
-                .show();
+                dateAndTime.get(Calendar.DAY_OF_MONTH)).show();
     }
     DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -50,12 +98,29 @@ public class CreateChartActivity extends AppCompatActivity {
         }
     };
     private void setInitialDateTime() {
+        birthDate = findViewById(R.id.editTextBirthDate);
         birthDate.setText(String.valueOf(DateUtils.formatDateTime(this, dateAndTime.getTimeInMillis(), DateUtils.FORMAT_SHOW_DATE)));
     }
     public void onSkipChartClick(View v){
         startActivity(new Intent(this, MainActivity.class));
     }
     public void onCreateChartClick(View v){
+        editTextAddress = findViewById(R.id.editTextAddress);
+        editTextSurname = findViewById(R.id.editTextSurname);
+        editTextLastname = findViewById(R.id.editTextLastname);
+        editTextBirthDate = findViewById(R.id.editTextBirthDate);
+        spinner = findViewById(R.id.spinnerGender);
+
+        sharedPreferences = getSharedPreferences("CHART", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("address", editTextAddress.getText().toString());
+        editor.putString("surname", editTextSurname.getText().toString());
+        editor.putString("lastname", editTextLastname.getText().toString());
+        editor.putString("birthdate", editTextBirthDate.getText().toString());
+        editor.putString("gender", spinner.getSelectedItem().toString());
+        editor.commit();
+
         startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/smartlab/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/smartlab/fragments/ProfileFragment.java
@@ -1,53 +1,41 @@
 package com.example.smartlab.fragments;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.DatePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.smartlab.R;
+import com.example.smartlab.activities.CreateChartActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Calendar;
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public ProfileFragment() {}
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
+    public static ProfileFragment newInstance() {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,27 +43,114 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
+    TextView birthDate;
+    Calendar dateAndTime = Calendar.getInstance();
+    EditText editTextAddress, editTextSurname, editTextLastname;
+    TextView editTextBirthDate;
+    Spinner spinner;
+    Button buttonSaveOrCreate;
+
+    String[] genders = {"Мужской", "Женский"};
+
+    SharedPreferences sharedPreferences;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        String[] genders = {"М", "Ж"};
-
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        Spinner spinner = view.findViewById(R.id.spinnerGender);
+
+        editTextAddress = view.findViewById(R.id.editTextAddress);
+        editTextSurname = view.findViewById(R.id.editTextSurname);
+        editTextLastname = view.findViewById(R.id.editTextLastname);
+        editTextBirthDate = view.findViewById(R.id.editTextBirthDate);
+        spinner = view.findViewById(R.id.spinnerGender);
+
+        sharedPreferences = getContext().getSharedPreferences("CHART", MODE_PRIVATE);
+        if(sharedPreferences.contains("surname")) {
+            editTextSurname.setText(sharedPreferences.getString("surname", ""));
+            buttonSaveOrCreate = view.findViewById(R.id.buttonSaveOrCreate);
+            buttonSaveOrCreate.setText("Сохранить");
+        }
+        if(sharedPreferences.contains("lastname")) editTextLastname.setText(sharedPreferences.getString("lastname", ""));
+        if(sharedPreferences.contains("birthdate")) editTextBirthDate.setText(sharedPreferences.getString("birthdate", ""));
+        if(sharedPreferences.contains("address")) editTextAddress.setText(sharedPreferences.getString("address", ""));
         ArrayAdapter<String> adapter = new ArrayAdapter(getContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, genders);
         spinner.setAdapter(adapter);
+        if(sharedPreferences.contains("gender")){
+            if(sharedPreferences.getString("gender", "Женский").equals("Женский")) spinner.setSelection(1);
+            else spinner.setSelection(0);
+        }
+
+            TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String addressText = editTextAddress.getText().toString();
+                String surnameText = editTextSurname.getText().toString();
+                String lastnameText = editTextLastname.getText().toString();
+                String birthdateText = editTextBirthDate.getText().toString();
+                buttonSaveOrCreate = view.findViewById(R.id.buttonSaveOrCreate);
+                if (!TextUtils.isEmpty(addressText) && !TextUtils.isEmpty(surnameText) && !TextUtils.isEmpty(lastnameText) && !TextUtils.isEmpty(birthdateText)) {
+                    buttonSaveOrCreate.setBackground(getContext().getDrawable(R.drawable.rounded_button_active));
+                    buttonSaveOrCreate.setEnabled(true);
+                }
+                else {
+                    buttonSaveOrCreate.setBackground(getContext().getDrawable(R.drawable.rounded_button_inactive));
+                    buttonSaveOrCreate.setEnabled(false);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        };
+        editTextAddress.addTextChangedListener(textWatcher);
+        editTextSurname.addTextChangedListener(textWatcher);
+        editTextLastname.addTextChangedListener(textWatcher);
+        editTextBirthDate.addTextChangedListener(textWatcher);
+
+        buttonSaveOrCreate = view.findViewById(R.id.buttonSaveOrCreate);
+        buttonSaveOrCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedPreferences = getContext().getSharedPreferences("CHART", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("address", editTextAddress.getText().toString());
+                editor.putString("surname", editTextSurname.getText().toString());
+                editor.putString("lastname", editTextLastname.getText().toString());
+                editor.putString("birthdate", editTextBirthDate.getText().toString());
+                editor.putString("gender", spinner.getSelectedItem().toString());
+                editor.commit();
+
+                Toast.makeText(getContext(), "Изменения сохранены", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         ImageView profilePic = view.findViewById(R.id.imgProfilePicture);
         profilePic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getContext(), "ну картинка типо", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        birthDate = view.findViewById(R.id.editTextBirthDate);
+        DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                dateAndTime.set(Calendar.YEAR, year);
+                dateAndTime.set(Calendar.MONTH, monthOfYear);
+                dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                birthDate = view.findViewById(R.id.editTextBirthDate);
+                birthDate.setText(String.valueOf(DateUtils.formatDateTime(getContext(), dateAndTime.getTimeInMillis(), DateUtils.FORMAT_SHOW_DATE)));
+            }
+        };
+        birthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(getContext(), listener,
+                        dateAndTime.get(Calendar.YEAR),
+                        dateAndTime.get(Calendar.MONTH),
+                        dateAndTime.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
         return view;
